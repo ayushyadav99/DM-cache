688602e6e8b7ca20f3d52628bff3e5dff66a9073
brahmakulkarni <brahma.kulkarni@iiitb.org]>
2018-11-22 10:43:53 +0530

    read memory successfully

--- cache.v
+++ cache.v
@@ -1,18 +1,18 @@
-module comparator(address, tag_reg, flag, clk);
-    input[31:0] address;
-    input clk;
-    input [19:0]tag_reg;
-    output reg flag;
-    always @ (posedge clk)
-            begin
-            if (tag_reg == tag)
-                begin
-                    flag =1;
-                end
-            else
-                flag = 0;
-            end
-endmodule
+// module comparator(address, tag_reg, flag, clk);
+//     input[31:0] address;
+//     input clk;
+//     input [19:0]tag_reg;
+//     output reg flag;
+//     always @ (posedge clk)
+//             begin
+//             if (tag_reg == tag)
+//                 begin
+//                     flag =1;
+//                 end
+//             else
+//                 flag = 0;
+//             end
+// endmodule
 
 module cache(address, hit_miss, out, clk);
     // reg [31:0]cache[256*16-1:0]
@@ -26,12 +26,12 @@ module cache(address, hit_miss, out, clk);
     reg [31:0]memory[29999:0];
     initial begin
         $readmemb("memory.txt",memory);
+        $display("%b", memory[34]);
     end
-    // $display("%b\n", memory[0]);
 
-    reg [3:0]block_offset = address[3:0];
-    reg [7:0]index = address[11:4];
-    reg [19:0]tag = address[31:12];
+    wire [3:0]block_offset = address[3:0];
+    wire [7:0]index = address[11:4];
+    wire [19:0]tag = address[31:12];
 
     always @ (posedge clk)
         begin
