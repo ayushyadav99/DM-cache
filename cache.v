c15a671dd0d73176bb76665b012f190046e8b125
brahmakulkarni <brahma.kulkarni@iiitb.org]>
2018-11-22 10:10:06 +0530

    finished hit logic

--- cache.v
+++ cache.v
@@ -2,7 +2,7 @@ module comparator(address, tag_reg, flag, clk);
     input[31:0] address;
     input clk;
     input [19:0]tag_reg;
-    output reg flag;    
+    output reg flag;
     always @ (posedge clk)
             begin
             if (tag_reg == tag)
@@ -18,10 +18,12 @@ module cache(address, hit_miss, out, clk);
     // reg [31:0]cache[256*16-1:0]
     input [31:0]address;
     input clk;
-    output hit_miss;
-    output [31:0]out;
+    output reg hit_miss;                    //1 if hit, 0 if miss
+    output reg [31:0]out;
+    
     reg [31:0][15:0]cache[255:0];
-    reg [19:0]tag_reg[255:0]; //for cache
+    reg [19:0]tag_reg[255:0];               //for cache
+    
     reg [3:0]block_offset = address[3:0];
     reg [7:0]index = address[11:4];
     reg [19:0]tag = address[31:12];
@@ -29,11 +31,16 @@ module cache(address, hit_miss, out, clk);
     always @ (posedge clk)
         begin
             if (tag_reg[index] == tag)
-                hit_miss = 1;
+                begin
+                    hit_miss <= 1;
+                    out <= cache[index][block_offset];
+                end
             else
-                hit_miss = 0;
+                begin
+                    hit_miss = 0;
+                end
         end
-   
 
-    
+
+
 endmodule
